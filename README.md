e50e80344f13028c9caefb9fa89a3a2a5ee930d0
Ben Douglas <bdouglas@onshape.com>
2016-04-14 12:27:03 -0400

    Initial app-qa code

---
+++ README.md
@@ -0,0 +1,116 @@
+#**app-qa**
+Sample app for QA
+
+App QA is an node.js application demonstrating basic integrated app permissions
+
+####**Using QA**
+Go to your [app preferences](https://partner.dev.onshape.com/). Click the "Create application" within Onshape document. Fill out the form like so:
+
+* **Name**: My QA app
+* **URL**: https://onshape-app-qa.herokuapp.com/oauthSignin
+* **Base HREF**: You can leave this blank
+
+This app requires to be run in a tab of Onshape, an iFrame. In this type of configuration, Onshape will pass documentId, workspaceId and elementId as query params to the frame. These are utilized by the QA app to give it context of what the active document is within Onshape.
+
+QA could also be written to run independently of the tab in Onshape. It could connect to Onshape and get a list of documents for the currently logged in user and then allow the user to select which one to work with.
+  
+
+####**Deploying to Heroku**
+Make sure you have Node.js and the Heroku Toolbelt installed. You will also need a Heroku account ([signup for free](https://www.heroku.com/)).
+
+Execute the following commands to create a duplicate of a repository; you need to perform both a bare-clone and a mirror-push to an newly-created bare repo (please note that you may want to use SSH instead of HTTPS, depending on your Github settings):
+
+    $ git clone --bare https://github.com/onshape/app-qa.git
+       # make a bare clone of the repository
+    
+    $ cd app-qa.git
+    $ git push --mirror https://github.com/exampleuser/new-respository.git
+       # mirror-push to new respository
+       
+    $ cd ..
+    $ rm -rf app-qa.git
+      # remove temporary local repository
+
+######Deploy your repo on heroku
+
+    $ git clone https://github.com/exampleuser/new-respository.git 
+    $ cd new-repository
+    $ heroku create
+
+To regsister the new app, go to the Dev Portal, https://dev-portal.dev.onshape.com. The output from Heroku should produce the domain name:
+
+    Application name (ex: Onshape QA Sample)
+    Application description (one sentence; ex: "Onshape QA Sample application — source code is available.")
+    URL for sign-in (ex: onshape-app-qa.herokuapp.com/oauthSignin)
+    URL for redirect (ex: onshape-app-qa.herokuapp.com/oauthRedirect)
+    Requested Format ID (ex: Onshape-Demo/QA)
+
+Onshape will register the app on Partner server and send back the OAUTH ID/Secret which are required for authentication.
+
+Make changes to code at two places for the new URL that Heroku has produced, as shown below:
+
+    file# 1: ./package.json
+       
+       ......... 
+       ........
+       "repository": {
+       "type": "git",
+       "url": "https://newURL-from-heroku.herokuapp.com/"
+       },
+       ...........
+       
+   And
+   
+    file# 2: ./authentication.js
+          
+        ........... 
+       passport.use(new OnshapeStrategy({
+         clientID: oauthClientId,
+         clientSecret: oauthClientSecret,
+         callbackURL: "https://newURL-from-heroku.herokuapp.com/oauthRedirect",
+         .............
+       },
+       function(accessToken, refreshToken, profile, done) {
+         ........... 
+
+Push the local repo code along with code changes to heruko
+
+    $ git add package.json
+    $ git add authentication.js
+    $ git commit -am "changes to code for callbackURL"
+    
+    $ git push heroku master
+
+You will need to set the ID and Secret as environment variables on the server. These are only visible to the app running on the server preserving security of that information.
+
+    $ heroku config:set OAUTH_CLIENT_ID=<ID given by Onshape for this app>
+    $ heroku config:set OAUTH_CLIENT_SECRET=<Secret given by Onshape for this app>
+
+You can verify that they are set by calling this:
+
+    $ heroku config
+
+One more step before you can use this app sample with Onshape. It requires RedisTOGO. 
+
+    $ heroku addons:create redistogo
+
+If you are new to Heroku, it may complain the first time you do this for an app requiring you to add credit card info as a payment source for potential server traffic. Don't worry, you can select the level of service for RedisTOGO and the base level is free (no cost). The payment source is required in case the service is scaled up to handle a large number of users. You do this via www.heroku.com.
+
+Use heroku config again to verify that RedisTOGO is setup. You'll see this in the config.
+
+    REDISTOGO_URL:        redis://redistogo:bb0854dd586250250969a8b0ea4aa695@hammerjaw.redistogo.com:11093/
+    
+
+
+#####**Reference Documentation**
+######***Heroku***
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+ -  [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+ -  [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+ -  [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
+ 
+######***OAuth***
+Onshape uses standard OAuth2. 
+ - [See the RFC for a detailed description of OAuth] (https://tools.ietf.org/html/rfc6749) 
+ - [Digital Ocean provides a nice tutorial on using OAuth] (https://www.digitalocean.com/community/tutorials/an-introduction-to-oauth-2)
---
+++ routes/api.js
@@ -0,0 +1,578 @@
+var express = require('express');
+var session = require('express-session');
+var redis = require('redis');
+
+var router = express.Router();
+var authentication = require('../authentication');
+var request = require('request-promise');
+var url = require('url');
+
+var platformPath = process.env.ONSHAPE_PLATFORM;
+console.log('Platform Path :: ', platformPath);
+
+var client;
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  client = require("redis").createClient(rtg.port, rtg.hostname);
+
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  client = redis.createClient();
+}
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.status(401).send({
+    authUri: authentication.getAuthUri(),
+    msg: 'Authentication required.'
+  });
+}
+
+router.sendNotify = function(req, res) {
+  if (req.body.event == 'onshape.model.lifecycle.changed') {
+    var state = {
+      elementId : req.body.elementId,
+      change : true
+    };
+
+    var stateString = JSON.stringify(state);
+    var uniqueID = "change" + req.body.elementId;
+    client.set(uniqueID, stateString);
+  }
+
+  res.send("ok");
+}
+
+router.post('/logout', function(req, res) {
+  req.session.destroy();
+  return res.send({});
+});
+
+var getSession = function(req, res) {
+  request.get({
+    uri: platformPath + '/api/users/sessioninfo',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    console.log('****** getSession - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getSession(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting session: ', err);
+      });
+    } else {
+      console.log('GET /api/users/session error: ', data);
+    }
+  });
+};
+
+var apiCallFail = function(error, response, message, requestBody, responseBody) {
+  var failMessage = 'API Call failed';
+  if (error) {
+    return error
+  } else if (response && response.statusCode) {
+    failMessage = message + ':\n[ ' + response.statusCode + ' ] ' +
+      response.request.method + ' ' + response.request.uri.href;
+    if (global.flags.verbose) {
+      if (requestBody) {
+        failMessage += '\n\n>>>> REQUEST:\n' + JSON.stringify(requestBody, null, 2);
+      }
+      if (responseBody) {
+        failMessage += '\n\n<<<< RESPONSE:\n' + JSON.stringify(responseBody, null, 2);
+      }
+    }
+    return { error: true, _statusCode: response.statusCode, message: failMessage };
+  }
+};
+
+var getDocuments = function(req, res) {
+  var next = true;
+  var resourceList = [ ];
+  var deferred = Promise.defer();
+
+  var options = {
+    uri: platformPath + '/api/documents',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  };
+
+  async.whilst(function() {
+    return next;
+  }, function(nextIteration) {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        next = false; // break out of the whilst loop
+        nextIteration(apiCallFail(error, response, 'Expected resource list to succeed', options.body, body));
+      } else {
+        if (body.next && body.next !== options.url) {
+          options.url = body.next;
+        } else {
+          next = false;
+        }
+        resourceList = resourceList.concat(body.items);
+        nextIteration();
+      }
+    });
+  }, function(error) {
+    if (error) {
+      deferred.reject(error);
+    } else {
+      deferred.resolve(resourceList);
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getDocuments(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting documents: ', err);
+      });
+    } else {
+      console.log('GET /api/documents error: ', data);
+    }
+  });
+};
+
+var getElementList = function(req, res) {
+  var url = platformPath + '/api/documents/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/elements';
+  if (req.query.elementId) {
+    url += '/?elementId=' + req.query.elementId;
+  }
+
+  request.get({
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    console.log('****** getElementList - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getElementList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting elements: ', err);
+      });
+    } else {
+      console.log('GET /api/documents/elements error: ', data);
+    }
+  });
+};
+
+var getAssemblyList = function(req, res) {
+  var url = platformPath + '/api/documents/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/elements?elementType=assembly';
+  if (req.query.elementId) {
+    url += '/?elementId=' + req.query.elementId;
+  }
+
+  request.get({
+    uri: url,
+    headers: {
+           'Authorization': 'Bearer ' + req.user.accessToken
+         }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    console.log('****** getAssemblyList - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getElementList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting elements: ', err);
+      });
+    } else {
+      console.log('GET /api/documents/elements ?type=assembly error: ', data);
+    }
+  });
+};
+
+
+var getShadedView = function(req, res) {
+  request.get({
+    uri: platformPath + '/api/assemblies/d/' + req.query.documentId +
+    '/w/' + req.query.workspaceId + '/e/' + req.query.elementId + '/shadedviews?' +
+    '&outputHeight=' + req.query.outputHeight + '&outputWidth=' + req.query.outputWidth + '&pixelSize=' + req.query.pixelSize +
+    '&viewMatrix=' + req.query.viewMatrix1 + ',' + req.query.viewMatrix2 + ',' + req.query.viewMatrix3 + ',' + req.query.viewMatrix4 +
+    ',' + req.query.viewMatrix5 + ',' + req.query.viewMatrix6 + ',' + req.query.viewMatrix7 + ',' + req.query.viewMatrix8 +
+    ',' + req.query.viewMatrix9 + ',' + req.query.viewMatrix10 + ',' + req.query.viewMatrix11 + ',' + req.query.viewMatrix12 +
+    '&perspective=false',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getShadedView(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting shaded view: ', err);
+      });
+    } else {
+      console.log('GET /api/assemblies/shadedviews error: ', data);
+    }
+  });
+};
+
+var getBoundingBox = function(req, res) {
+  request.get({
+    uri: platformPath + '/api/assemblies/d/' + req.query.documentId +
+          '/w/' + req.query.workspaceId + '/e/' + req.query.elementId + '/boundingboxes/',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getBoundingBox(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting bounding box: ', err);
+      });
+    } else {
+      console.log('GET /api/assemblies/boundingbox error: ', data);
+    }
+  });
+};
+
+var getPartsList = function(req, res) {
+  request.get({
+    uri: platformPath + '/api/parts/d/' + req.query.documentId + '/w/' + req.query.workspaceId,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getPartsList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting parts: ', err);
+      });
+    } else {
+      console.log('GET /api/parts/workspace error: ', data);
+    }
+  });
+};
+
+var getAssemblyDefinition = function(req, res) {
+  request.get({
+    uri: platformPath + '/api/assemblies/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/e/' + req.query.nextElement + '?includeMateFeatures=false',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getAssemblyDefinition(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting assembly definition: ', err);
+      });
+    } else {
+      console.log('GET /api/models/assembly/definition error: ', data);
+    }
+  });
+};
+
+var getMetadata = function(req, res) {
+   request.get({
+    uri:platformPath + '/api/parts/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/e/' + req.query.elementId + '/partid/' + req.query.partId + '/metadata',
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getMetadata(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting part metadata: ', err);
+      });
+    } else {
+      console.log('GET /api/parts/metadata error: ', data);
+    }
+  });
+};
+
+var getStudioMetadata = function(req, res) {
+  var url = '/api/partstudios/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/e/' + req.query.elementId + '/metadata';
+  if (req.query.microversionId > 0) {
+    url = '/api/partstudios/d/' + req.query.documentId + '/m/' + req.query.microversionId + '/e/' + req.query.elementId + '/metadata';
+  }
+  request.get({
+    uri:platformPath + url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getMetadata(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting partstudio metadata: ', err);
+      });
+    } else {
+      console.log('GET /api/partstudios/metadata error: ', data);
+    }
+  });
+};
+
+var getExternalStudioMetadata = function(req, res) {
+  var url = '/api/partstudios/d/' + req.query.documentId + '/v/' + req.query.versionId + '/e/' + req.query.elementId + '/metadata?linkDocumentId=' + req.query.linkDocumentId;
+  request.get({
+    uri:platformPath + url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getMetadata(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting external partstudio metadata: ', err);
+      });
+    } else {
+      console.log('GET /api/partstudios/metadata error: ', data);
+    }
+  });
+};
+
+var setWebhooks = function(req, res) {
+  var eventList = [ "onshape.model.lifecycle.changed" ];
+  var options = { collapseEvents : true };
+  var urlNotify = "https://onshape-app-qa.herokuapp.com/notify";
+  var filter = "{$DocumentId} = '" + req.query.documentId + "' && " +
+               "{$WorkspaceId} = '" + req.query.workspaceId + "' && " +
+               "{$ElementId} = '" + req.query.elementId + "'";
+
+  request.post({
+    uri: platformPath + '/api/webhooks/',
+    body: {
+      url : urlNotify,
+      events : eventList,
+      filter : filter,
+      options : options
+    },
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    },
+    json : true
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        setWebhooks(req, res);
+      }).catch(function(err) {
+        console.log('*** Error refreshing token or setting webhooks: ', err);
+      });
+    } else {
+      console.log('*** POST /api/webhooks error: ', data);
+    }
+  });
+};
+
+var checkModelChange = function(req, res) {
+  var data = {
+    statusCode : 200,
+    change : false
+  };
+
+  // Get the current setting from Redis (if there is one)
+  var uniqueID = "change" + req.query.elementId;
+  client.get(uniqueID, function(err, reply) {
+    // reply is null when the key is missing
+    if (reply != null) {
+      var newParams = JSON.parse(reply);
+      data.change = newParams.change;
+
+      // Now that we have the value, clear it in Redis
+      var state = {
+        elementId : req.query.elementId,
+        change : false
+      };
+
+      var stateString = JSON.stringify(state);
+      client.set(uniqueID, stateString);
+    }
+
+    res.send(data);
+  });
+}
+
+var getAccounts = function(req, res) {
+  var url = process.env.ONSHAPE_PLATFORM + '/api/accounts/purchases';
+
+  request.get({
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    var object = JSON.parse(data);
+
+    // Walk through the various apps that the user has purchased looking for this one.
+    var isSubscribed = false;
+    for (var i = 0; i < object.length; ++i) {
+      if (object[i].clientId == process.env.OAUTH_CLIENT_ID)
+        isSubscribed = true;
+    }
+
+    var returnData = {
+      Subscribed : isSubscribed,
+      Items : object
+    };
+
+    res.send(returnData);
+  }).catch(function(data) {
+    console.log('****** getAccounts - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getElementList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting accounts: ', err);
+      });
+    } else {
+      console.log('GET /api/accounts/purchases error: ', data);
+    }
+  });
+};
+
+var getWorkspace = function(req, res) {
+  var url = process.env.ONSHAPE_PLATFORM + '/api/documents/d/' + req.query.documentId + '/workspaces';
+
+  request.get({
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    console.log('****** getWorkspace - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getElementList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting workspace: ', err);
+      });
+    } else {
+      console.log('GET /api/documents/workspaces error: ', data);
+    }
+  });
+};
+
+var getVersions = function(req, res) {
+  var url = process.env.ONSHAPE_PLATFORM + '/api/documents/d/' + req.query.documentId + '/versions';
+
+  request.get({
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    console.log('****** getVersions - CATCH ' + data.statusCode);
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        getElementList(req, res);
+      }).catch(function(err) {
+        console.log('Error refreshing token or getting versions: ', err);
+      });
+    } else {
+      console.log('GET /api/documents/versions error: ', data);
+    }
+  });
+};
+
+var addPartStudio = function(req, res) {
+  request.post({
+    uri: process.env.ONSHAPE_PLATFORM + '/api/partstudios/d/' + req.query.documentId + '/w/' + req.query.workspaceId,
+    body: {
+      name : req.query.name
+    },
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    },
+    json: true
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        addPartStudio(req, res);
+      }).catch(function(err) {
+        console.log('*** Error refreshing token or inserting new partstudio: ', err);
+      });
+    } else {
+      console.log('*** POST /api/partstudios error: ', data);
+    }
+  });
+};
+
+var delElement = function(req, res) {
+  request.del({
+    uri: process.env.ONSHAPE_PLATFORM + '/api/documents/d/' + req.query.documentId + '/w/' + req.query.workspaceId + '/e/' + req.query.elementId,
+    headers: {
+      'Authorization': 'Bearer ' + req.user.accessToken
+    }
+  }).then(function(data) {
+    res.send(data);
+  }).catch(function(data) {
+    if (data.statusCode === 401) {
+      authentication.refreshOAuthToken(req, res).then(function() {
+        delElement(req, res);
+      }).catch(function(err) {
+        console.log('*** Error refreshing token or deleting element: ', err);
+      });
+    } else {
+      console.log('*** DELETE /api/documents error: ', data);
+    }
+  });
+}
+
+router.get('/documents', getDocuments);
+router.get('/session', getSession);
+router.get('/elements', getElementList);
+router.get('/assemblies', getAssemblyList);
+router.get('/parts', getPartsList);
+router.get('/boundingBox', getBoundingBox);
+router.get('/definition', getAssemblyDefinition);
+router.get('/shadedView', getShadedView);
+router.get('/metadata', getMetadata);
+router.get('/studiometadata', getStudioMetadata);
+router.get('/externalstudiometadata', getExternalStudioMetadata);
+router.get('/webhooks', setWebhooks);
+router.get('/modelchange', checkModelChange);
+router.get('/accounts', getAccounts);
+router.get('/workspace', getWorkspace);
+router.get('/versions', getVersions);
+router.get('/newps', addPartStudio);
+router.get('/delelement', delElement);
+
+module.exports = router;
